1132dfc534edf13319fa844de873cf83d9f8d87f
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-28 14:53:12 +0530

    Added new files and updates

---
+++ src/router/ping_pong.js
@@ -0,0 +1,55 @@
+ 
+const  ping_pong = require('express').Router();
+const fetch = require('node-fetch'); 
+let resolve=false;
+ping_pong.get('/ping', async (req, res) => {
+    console.log('Pong Pong Server 2');
+  res.send('Pong from Server 2');
+  if(resolve==true){
+    resolve=false;
+  setTimeout(() => { 
+    callback();
+  }, 300000);
+  }
+});
+
+
+(async () => {
+  resolve=true
+   await fetch('https://server.markethealers.com/ping')
+     .then((res) => {
+       if (res.ok) {
+         console.log('Server 1 is responding:', res.status);
+       } else {
+         console.log('Server 1 responded with an error:', res.status);
+          setTimeout(() => {
+            callback();
+          }, 10000);
+       }
+     })
+     .catch((err) => {
+       console.error('Server 1 is not responding:', err.message);
+     });
+})()
+
+
+
+
+ function callback() {
+  resolve=true
+ fetch('https://server.markethealers.com/ping')
+   .then((res) => {
+     if (res.ok) {
+       console.log('Server 1 is responding:', res.status);
+     } else {
+       console.log('Server 1 responded with an error:', res.status);
+     }
+   })
+   .catch((err) => {
+     console.error('Server 1 is not responding:', err.message);
+   });
+}
+  
+ ping_pong.use((req,res)=>res.send("Hello from server 2"));
+
+ module.exports= ping_pong;
