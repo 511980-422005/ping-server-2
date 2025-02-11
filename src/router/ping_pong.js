c31cab46380ade3d8ab674b1b3fecdb85943bd1a
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-11 20:27:53 +0530

    dd

--- src/router/ping_pong.js
+++ src/router/ping_pong.js
@@ -1,53 +1,53 @@
- 
-const  ping_pong = require('express').Router();
-const fetch = require('node-fetch'); 
-let resolve=true;
+const ping_pong = require('express').Router();
+const fetch = require('node-fetch');
+let isPing = false;
 ping_pong.get('/ping', async (req, res) => {
-    console.log('Pong Pong Server 2');
-  res.send('Pong from Server 2');
-  if(resolve==true){
-    resolve=true;
-  setTimeout(() => { 
-    callback();
-  }, 300000);
+  if (isPing == true) {
+    isPing = false;
+    setTimeout(() => {
+      callback();
+    }, 5 * 60 * 1000);
   }
+  console.log('Pong Pong Server 2');
+  res.send('Pong from Server 2');
 });
- 
+
 (async () => {
-  resolve=true
-   await fetch('https://server.markethealers.com/ping')
-     .then((res) => {
-       if (res.ok) {
-         console.log('Server 1 is responding:', res.status);
-       } else {
-         console.log('Server 1 responded with an error:', res.status);
-          setTimeout(() => {
-            callback();
-          }, 10000);
-       }
-     })
-     .catch((err) => {
-       console.error('Server 1 is not responding:', err.message);
-     });
-})()
+  await fetch('https://server.markethealers.com/ping')
+    .then((res) => {
+      if (res.ok) {
+        console.log('Server 1 is responding:', res.status);
+        isPing = true;
+      } else {
+        console.log('Server 1 responded with an error:', res.status);
+        callback();
+      }
+    })
+    .catch((err) => {
+      console.error('Server 1 is not responding:', err.message);
+      callback();
+    });
+})();
 
-
-
-
- function callback() {
-  resolve=true
- fetch('https://server.markethealers.com/ping')
-   .then((res) => {
-     if (res.ok) {
-       console.log('Server 1 is responding:', res.status);
-     } else {
-       console.log('Server 1 responded with an error:', res.status);
-     }
-   })
-   .catch((err) => {
-     console.error('Server 1 is not responding:', err.message);
-   });
+function callback() {
+  fetch('https://server.markethealers.com/ping')
+    .then((res) => {
+      if (res.ok) {
+        console.log('Server 1 is responding:', res.status);
+        isPing = true;
+      } else {
+        console.log('Server 1 responded with an error:', res.status);
+        setTimeout(() => {
+          callback();
+        }, 5 * 60 * 1000);
+      }
+    })
+    .catch((err) => {
+      console.error('Server 1 is not responding:', err.message);
+      setTimeout(() => {
+        callback();
+      }, 5 * 60 * 1000);
+    });
 }
-   
 
- module.exports= ping_pong;
+module.exports = ping_pong;
