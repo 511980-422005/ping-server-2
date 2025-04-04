35d3b1916c67ab5585c12d0d134391cb50c37887
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-04-04 09:17:53 +0530

    d

--- ping_pong.js
+++ ping_pong.js
@@ -2,68 +2,45 @@ const ping_pong = require('express').Router();
 const fetch = require('node-fetch');
 let isPing = false;
 
+const mainServer = 'https://server.markethealers.com';
+const otherServers = [
+  'https://edubuddy-backend-server.onrender.com',
+  'https://server-guard-server.onrender.com',
+];
+
 ping_pong.get('/ping', async (req, res) => {
-  if (isPing == true) {
+  if (isPing) {
     isPing = false;
     setTimeout(() => {
-      callback();
+      pingServers();
     }, 5 * 60 * 1000);
   }
   console.log('Pong Pong Server 2');
   res.send('Pong from Server 2');
 });
 
+const pingServers = () => {
+  fetch(`${mainServer}/ping`)
+    .then((res) => {
+      if (res.ok) {
+        console.log(`Main server is responding:`, res.status);
+        isPing = true;
+      } else {
+        console.log(`Main server responded with an error:`, res.status);
+        setTimeout(pingServers, 5 * 60 * 1000);
+      }
+    })
+    .catch((err) => {
+      console.error(`Main server is not responding:`, err.message);
+      setTimeout(pingServers, 5 * 60 * 1000);
+    });
+  for (const server of otherServers) {
+    fetch(`${server}/ping`).catch(() => {});
+  }
+};
+
 (async () => {
-   fetch('https://server.markethealers.com/ping')
-    .then((res) => {
-      if (res.ok) {
-        console.log('Server 1 is responding:', res.status);
-        isPing = true;
-      } else {
-        console.log('Server 1 responded with an error:', res.status);
-        callback();
-      }
-    })
-    .catch((err) => {
-      console.error('Server 1 is not responding:', err.message);
-      callback();
-    });
-
-      fetch('https://server-guard-server.onrender.com/ping')
-       .then((res) => {
-          
-       })
-       .catch((err) => {
-         
-       }); 
+  pingServers();
 })();
-
-function callback() {
-  fetch('https://server.markethealers.com/ping')
-    .then((res) => {
-      if (res.ok) {
-        console.log('Server 1 is responding:', res.status);
-        isPing = true;
-      } else {
-        console.log('Server 1 responded with an error:', res.status);
-        setTimeout(() => {
-          callback();
-        }, 5 * 60 * 1000);
-      }
-    })
-    .catch((err) => {
-      console.error('Server 1 is not responding:', err.message);
-      setTimeout(() => {
-        callback();
-      }, 5 * 60 * 1000);
-    });
-    fetch('https://zenova-server.onrender.com/ping')
-       .then((res) => {
-          
-       })
-       .catch((err) => {
-         
-       }); 
-}
 
 module.exports = ping_pong;
