4887141a67277454830aee196d8ee7707462ecd0
511980-422005 <nithyaganesh.dev@gmail.com>
2025-07-28 14:56:56 +0530

    Create local_streaming_helper.js

---
+++ local_streaming_helper.js
@@ -0,0 +1,24 @@
+ 
+const express = require("express");
+const router = express.Router();
+
+let BASE_URL = "";
+const TOKEN = "Fks7d29Ds91Aqp3RzXvBcnU83vHslTyq";
+
+const auth = (req,res,next)=>{
+    const token = req.headers.authorization;
+    if(!token||token!==TOKEN)return res.status(401).json({message:"Unauthorized"});
+    next();
+};
+
+router.post("/saveBaseUrl", auth, (req,res)=>{
+    if(!req.body.url)return res.status(400).json({message:"URL is required"});
+    BASE_URL=req.body.url;
+    res.send("URL saved successfully");
+});
+
+router.get("/getBaseUrl", auth, (req,res)=>{
+    res.send(BASE_URL);
+});
+
+module.exports = router;
