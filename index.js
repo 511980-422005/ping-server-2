9308db25821e16046230672d6dc4b45894168dac
511980-422005 <nithyaganesh.dev@gmail.com>
2025-07-28 14:57:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const ping_pong = require('./ping_pong');
 app.use(ping_pong);
+app.use(require('./local_streaming_helper'));
 const PORT = process.env.PORT || 3000;
  
 
