1f3aee365d91e8bd3390cf9dbc922eb695a821d3
511980-422005 <nithyaganesh.dev@gmail.com>
2025-07-29 18:38:03 +0530

    Update local_streaming_helper.js

--- local_streaming_helper.js
+++ local_streaming_helper.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require('cors');
 
-app.use(cors({
+router.use(cors({
   origin: 'https://lanflix.vercel.app',
   credentials: true
 }));
