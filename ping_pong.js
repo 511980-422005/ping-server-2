ceb85db51c304ab6a84e9000091fafe8c5736af6
511980-422005 <nithyaganesh.dev@gmail.com>
2025-08-05 23:40:50 +0530

    Update ping_pong.js

--- ping_pong.js
+++ ping_pong.js
@@ -2,17 +2,8 @@ const ping_pong = require('express').Router();
 const fetch = require('node-fetch');
 let isPing = false;
 
-const mainServer = 'https://server.markethealers.com';
+const mainServer = 'https://servermonitoringsystembyng.onrender.com';
 const otherServers = [  
-  'https://le-crown-interiors-backend.onrender.com',
-  'https://iverto.onrender.com', 
-  'https://invetorymanagementsystembyisai.onrender.com',
-  'https://alkarahm-admin.onrender.com',
-  'https://alkarahm.onrender.com',
-  'https://zenova-server.onrender.com',
-  'https://www.picloopz.com',
-  'https://admin.picloopz.com',
-  'https://picloopz-server.onrender.com',
   'https://servermonitoringsystembyng.onrender.com'
 ];
 
